0d4a398b23e62f5ea07c64bfd1dd1b393b9fb418
kirurr <timon09df@gmail.com>
2024-03-28 18:20:40 +0300

    feat: added validation

--- src/routes/mainRoute.ts
+++ src/routes/mainRoute.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import { imageRepo } from '../repo/imageRepo'
+import { Result, ValidationError, body, matchedData, validationResult } from 'express-validator'
 
 export const mainRoute = express.Router()
 
@@ -7,7 +8,37 @@ mainRoute.get('/', (_req: Request, res: Response) => {
 	res.render('index')
 })
 
-mainRoute.post('/', async (req: Request, res:Response) => {
+mainRoute.post('/', body('*').trim().escape().isLength({max: 256}).withMessage('Слишком длинный текст'), async (req: Request, res:Response) => {
+	const validationData = validationResult(req)
+
+	if(!validationData.isEmpty()) {
+		const errorsArray = validationData.array().map(error => {
+			if (error.type === 'field') {
+        const result: { message: string; field: string } = {
+          message: error.msg,
+          field: ''
+        }
+
+        switch (error.path) {
+          case 'name':
+            result.field = 'ФИО'
+            break
+          case 'sum':
+            result.field = 'Сумма'
+            break
+					case 'number':
+						result.field = 'Номер сертификата'
+						break
+        }
+
+        return result
+      }
+		})
+		return res.render('error', {errors: errorsArray})
+	}
+
+	req.body = matchedData(req)
+
 	const fileName = await imageRepo.makeImage(req.body.name, req.body.sum, req.body.number)
 	const data = imageRepo.makeData()
 
