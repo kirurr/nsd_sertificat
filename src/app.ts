e21e6e2c167d66e1a9f4ee1cec67f5a6f8bb55f2
kirurr <timon09df@gmail.com>
2024-04-19 00:43:33 +0300

    feat: added login system

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express from 'express'
-import {mainRoute} from './routes/mainRoute'
+import { mainRoute } from './routes/mainRoute'
+import cookieParser from 'cookie-parser'
+import 'dotenv/config'
 
 export const app = express()
 
 app.set('view engine', 'pug')
 app.set('views', './src/views')
 
+app.use(cookieParser(process.env.COOKIE_SECRET))
+
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/', express.static('public'))
 
--- src/routes/mainRoute.ts
+++ src/routes/mainRoute.ts
@@ -1,53 +1,91 @@
 import express, { Request, Response } from 'express'
 import { imageRepo } from '../repo/imageRepo'
-import { Result, ValidationError, body, matchedData, validationResult } from 'express-validator'
+import { body, matchedData, validationResult } from 'express-validator'
+import 'dotenv/config'
+
+type Prop = {
+  isAuth: boolean
+  error?: boolean
+}
 
 export const mainRoute = express.Router()
 
-mainRoute.get('/', (_req: Request, res: Response) => {
-	res.render('index')
+mainRoute.get('/', (req: Request, res: Response) => {
+  const prop: Prop = { isAuth: false }
+  if (req.signedCookies.isAuth) prop.isAuth = true
+
+  res.render('index', prop)
 })
 
-mainRoute.post('/', body('*').trim().escape().isLength({max: 256}).withMessage('Слишком длинный текст'), async (req: Request, res:Response) => {
-	const validationData = validationResult(req)
-
-	if(!validationData.isEmpty()) {
-		const errorsArray = validationData.array().map(error => {
-			if (error.type === 'field') {
-        const result: { message: string; field: string } = {
-          message: error.msg,
-          field: ''
-        }
-
-        switch (error.path) {
-          case 'name':
-            result.field = 'ФИО'
-            break
-          case 'sum':
-            result.field = 'Сумма'
-            break
-					case 'number':
-						result.field = 'Номер сертификата'
-						break
-        }
-
-        return result
-      }
-		})
-		return res.render('error', {errors: errorsArray})
+mainRoute.post(
+  '/login',
+  body('password').trim().escape().isLength({ max: 5, min: 5 }),
+  (req: Request, res: Response) => {
+		const prop: Prop = { isAuth: false, error: true }
+		const validationData = validationResult(req)
+		if(validationData.isEmpty()) {
+			if (matchedData(req).password === process.env.PASSWORD) {
+				res.cookie('isAuth', true, { httpOnly: true, signed: true })
+				prop.isAuth = true
+				prop.error = false
+			}
 	}
+    res.render('index', prop)
+  }
+)
 
-	req.body = matchedData(req)
+mainRoute.post(
+  '/',
+  body('*')
+    .trim()
+    .escape()
+    .isLength({ max: 256 })
+    .withMessage('Слишком длинный текст'),
+  async (req: Request, res: Response) => {
+    const validationData = validationResult(req)
 
-	const fileName = await imageRepo.makeImage(req.body.name, req.body.sum, req.body.number)
-	const data = imageRepo.makeData()
+    if (!validationData.isEmpty()) {
+      const errorsArray = validationData.array().map((error) => {
+        if (error.type === 'field') {
+          const result: { message: string; field: string } = {
+            message: error.msg,
+            field: ''
+          }
 
-	const table = {
-		num: req.body.number,
-		data: data[0],
-		name: req.body.name,
-		status: data[1],
-		sum: req.body.sum,
-	}
-	res.render('download', {image: fileName, table: table})
-})
+          switch (error.path) {
+            case 'name':
+              result.field = 'ФИО'
+              break
+            case 'sum':
+              result.field = 'Сумма'
+              break
+            case 'number':
+              result.field = 'Номер сертификата'
+              break
+          }
+
+          return result
+        }
+      })
+      return res.render('error', { errors: errorsArray })
+    }
+
+    req.body = matchedData(req)
+
+    const fileName = await imageRepo.makeImage(
+      req.body.name,
+      req.body.sum,
+      req.body.number
+    )
+    const data = imageRepo.makeData()
+
+    const table = {
+      num: req.body.number,
+      data: data[0],
+      name: req.body.name,
+      status: data[1],
+      sum: req.body.sum
+    }
+    res.render('download', { image: fileName, table: table })
+  }
+)
